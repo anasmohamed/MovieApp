3282bcad89fba6f6ef200f6cb1bdf77d417bfbd9
anasmohamed <anaskamal659@gmail.com>
2017-03-18 23:01:24 +0200

    update

--- app/src/main/java/com/example/android/movieapp/fragment/TopRatedFragment.java
+++ app/src/main/java/com/example/android/movieapp/fragment/TopRatedFragment.java
@@ -4,6 +4,6 @@ public class TopRatedFragment extends BaseFragment {
 
     @Override
     String getMoviesUrl() {
-        return "https://api.themoviedb.org/3/movie/top_rated?api_key=&language=en-US";
+        return "https://api.themoviedb.org/3/movie/top_rated?api_key=//Your API Key&language=en-US";
     }
 }

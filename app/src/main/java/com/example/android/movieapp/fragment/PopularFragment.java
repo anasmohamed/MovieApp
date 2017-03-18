67b585192555aa1254803598411c61c7ed33c464
anasmohamed <anaskamal659@gmail.com>
2017-03-18 23:00:07 +0200

    update

--- app/src/main/java/com/example/android/movieapp/fragment/PopularFragment.java
+++ app/src/main/java/com/example/android/movieapp/fragment/PopularFragment.java
@@ -7,7 +7,7 @@ public class PopularFragment extends BaseFragment {
 
     @Override
     String getMoviesUrl() {
-        return "https://api.themoviedb.org/3/movie/popular?api_key=fafafa7b34f6bc864117117864178666&language=en-US";
+        return "https://api.themoviedb.org/3/movie/popular?api_key=Your Api Key&language=en-US";
     }
 
 
